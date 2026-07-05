af9947fac60f173e51a2dd4e7a02c94e7bc3ffd4
Abhishek kumar <89969964+Abhishek-kumar-202063@users.noreply.github.com>
2026-07-05 23:57:13 +0530

    Update gsp751.sh

--- Arcade-Base-Camp-July/Interact with Terraform Modules/gsp751.sh
+++ Arcade-Base-Camp-July/Interact with Terraform Modules/gsp751.sh
@@ -55,6 +55,12 @@ module "test-vpc-module" {
 # [END vpc_custom_create]
 EOF_END
 
+# --- Install Terraform (fix) ---
+wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install -y terraform
+# --- end fix ---
+
 terraform init
 
 terraform apply --auto-approve
